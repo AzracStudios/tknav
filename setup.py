72eb926ddd140391b81c1dbbf82a1df6801dad62
AzracStudios <azracstudios@gmail.com>
2024-03-21 07:48:24 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="tknav",
+    packages=["tknav"],
+    version="1.0.0",  # Ideally should be same as your github release tag varsion
+    description="Simple navigation API for tkinter",
+    author="Adarsh Ravikumar",
+    author_email="azracstudios@gmail.com",
+    url="https://www.github.com/AzracStudios/tknav",
+    download_url="download link you saved",
+    keywords=["tkinter", "nav", "navigation", "tknav"],
+    classifiers=[],
+)
